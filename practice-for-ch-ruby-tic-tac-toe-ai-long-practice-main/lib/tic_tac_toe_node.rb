ab163ee373281b5c2802ad13bf76d9a39ec2e128
Levi Sherman <Levi Sherman>
2023-11-06 14:04:32 -0500

    find children of current board method created

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,16 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    possible_boards = []
+    board.rows.each.with_index do |row, row_idx|
+      row.each_index do |col_idx|
+        if row[col_idx].nil?
+          dup_board = board.dup
+          dup_board[[row_idx, col_idx]] = next_mover_mark
+          possible_boards << TicTacToeNode.new(dup_board, dup_board.next_mark, [row_idx, col_idx])
+        end
+      end
+    end
+    possible_boards
   end
 end
